6350a6c93fe5e0152d0dbf6a8f942ab649ba4a77
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-05-19 16:57:55 -0400

    update neuralnet code

--- neuralnet/run.sh
+++ neuralnet/run.sh
@@ -1,67 +1,69 @@
-# === IP ===
 
-# training
+# === Internet Traffic ===
 
-#python3 run_nn_cls.py \
-#    --train ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130000.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130100.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130200.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130300.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130400.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130500.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130600.ports.npy \
-#    --valid ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130700.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130800.ports.npy \
-#    --test  ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130900.ports.npy \
-#    --save exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 2000 --lr 0.0001 --regress_min 65
+## training
 
-#python3 run_nn_cls.py \
-#    --train ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130000.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130100.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130200.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130300.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130400.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130500.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130600.ports.npy \
-#    --valid ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130700.ports.npy \
-#            ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130800.ports.npy \
-#    --test  ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130900.ports.npy \
+python3 run_ip_model.py \
+   --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130100.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130200.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130300.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130400.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130500.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130600.ports.npy \
+   --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130800.ports.npy \
+   --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
+   --save exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 2000 --lr 0.0001 --regress_min 65
+
+## resume training from an earlier model checkpoint
+
+#python3 run_ip_model.py \
+#    --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
+#            ../data/equinix-chicago.dirA.20160121-130100.ports.npy \
+#            ../data/equinix-chicago.dirA.20160121-130200.ports.npy \
+#            ../data/equinix-chicago.dirA.20160121-130300.ports.npy \
+#            ../data/equinix-chicago.dirA.20160121-130400.ports.npy \
+#            ../data/equinix-chicago.dirA.20160121-130500.ports.npy \
+#            ../data/equinix-chicago.dirA.20160121-130600.ports.npy \
+#    --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
+#            ../data/equinix-chicago.dirA.20160121-130800.ports.npy \
+#    --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
 #    --save exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_ep100 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 2000 --lr 0.0001 --regress_min 65 \
 #    --resume model/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_20180505-214726_ep100.69 --start_epoch 101 --eval_n 10
 
-# evaluation
+## inference
 
-# gpu04 - 0 (tmux tab 2)
-#python3 run_nn_cls.py \
-#    --train ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130000.ports.npy \
-#    --valid ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130700.ports.npy \
-#    --test  ./data/2016/20160121-130000.UTC/equinix-chicago.dirA.20160121-130900.ports.npy \
+#python3 run_ip_model.py \
+#    --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
+#    --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
+#    --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
 #    --save pred_exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_ep350_1309 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 2000 --lr 0.0001 --regress_min 65 \
 #    --resume model/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_ep100_20180506-230316_ep350.69 --evaluate
-#
 
 
-# === AOL ===
 
-# training
+# === Search Query ===
 
-#python3 ./aol/run_aol_rnn.py \
-#    --train ./aol/data/aol/1-day_len60/aol_0000_len60.npz \
-#            ./aol/data/aol/1-day_len60/aol_0001_len60.npz \
-#            ./aol/data/aol/1-day_len60/aol_0002_len60.npz \
-#            ./aol/data/aol/1-day_len60/aol_0003_len60.npz \
-#            ./aol/data/aol/1-day_len60/aol_0004_len60.npz \
-#    --valid ./aol/data/aol/1-day_len60/aol_0005_len60.npz \
-#    --test  ./aol/data/aol/1-day_len60/aol_0006_len60.npz \
+## training
+
+#python3 ./run_aol_model.py \
+#    --train ../data/aol/char_embed/aol_0000_len60.npz \
+#            ../data/aol/char_embed/aol_0001_len60.npz \
+#            ../data/aol/char_embed/aol_0002_len60.npz \
+#            ../data/aol/char_embed/aol_0003_len60.npz \
+#            ../data/aol/char_embed/aol_0004_len60.npz \
+#    --valid ../data/aol/char_embed/aol_0005_len60.npz \
+#    --test  ../data/aol/char_embed/aol_0006_len60.npz \
 #    --save exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra  --embed_size 64 --rnn_hidden 256 --hiddens 32 --batch_size 128 --n_epoch 2000 --lr 0.0001 --word_max_len 60 --regress_actual --eval_n 10
 
+## evaluation
 
-# evaluation
-
-#python3 ./aol/run_aol_rnn.py \
-#    --train ./aol/data/aol/1-day_len60/aol_0000_len60.npz \
-#    --valid ./aol/data/aol/1-day_len60/aol_0005_len60.npz \
-#    --test  ./aol/data/aol/1-day_len60/aol_00${tday}_len60.npz \
+#tday=06
+#python3 ./run_aol_model.py \
+#    --train ../data/aol/char_embed/aol_0000_len60.npz \
+#    --valid ../data/aol/char_embed/aol_0005_len60.npz \
+#    --test  ../data/aol/char_embed/aol_00${tday}_len60.npz \
 #    --save aol_inf_all_v05_t${tday}_exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra_20180514-160509_ep190 --embed_size 64 --rnn_hidden 256 --hiddens 32 --batch_size 128 \
 #    --n_epoch 2000 --lr 0.0001 --word_max_len 60 --regress_actual \
 #    --resume models_aol/exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra_20180514-160509_ep190.69 --evaluate
--- neuralnet/run_aol_model.py
+++ neuralnet/run_aol_model.py
@@ -7,11 +7,9 @@ import datetime
 import numpy as np
 import tensorflow as tf
 
-from aol_utils import get_data_aol_feat_list
-
-sys.path.insert(0, '..')
-from utils import get_stat, git_log, AverageMeter, keep_latest_files
-from nn_utils import fc_layers, write_summary
+from utils.aol_utils import get_data_aol_feat_list
+from utils.utils import get_stat, git_log, AverageMeter, keep_latest_files
+from utils.nn_utils import fc_layers, write_summary
 
 
 def construct_graph(args):
@@ -87,9 +85,19 @@ def construct_graph(args):
         update_op = optimizer.minimize(loss)
         print('finished constructing the graph')
 
-    return feat, labels, learning_rate, update_op, loss, merged_sum, data_len, keep_probs, output
+    model = {}
+    model['feat'] = feat
+    model['labels'] = labels
+    model['learning_rate'] = learning_rate
+    model['update_op'] = update_op
+    model['loss'] = loss
+    model['merged_sum'] = merged_sum
+    model['data_len'] = data_len
+    model['keep_probs'] = keep_probs
+    model['output'] = output
+    return model
 
-def train(x, y, args, sess, ite, writer, idx=None):
+def train(model, x, y, args, sess, ite, writer, idx=None):
     if idx is None:
        idx = [i for i in range(len(y))]
        random.shuffle(idx)
@@ -108,13 +116,13 @@ def train(x, y, args, sess, ite, writer, idx=None):
             batch_y = np.concatenate((batch_y, np.zeros(args.batch_size - n_flows)), axis=0)
 
         input_feed = {
-            feat: batch_x,
-            labels: batch_y,
-            learning_rate: args.lr,
-            data_len: n_flows,
-            keep_probs: args.keep_probs,
+            model['feat']: batch_x,
+            model['labels']: batch_y,
+            model['learning_rate']: args.lr,
+            model['data_len']: n_flows,
+            model['keep_probs']: args.keep_probs,
             }
-        _, loss_b = sess.run([update_op, loss], feed_dict=input_feed)
+        _, loss_b = sess.run([model['update_op'], model['loss']], feed_dict=input_feed)
         loss_meter.update(loss_b)
 
         if ite % 100 == 0:
@@ -122,11 +130,11 @@ def train(x, y, args, sess, ite, writer, idx=None):
         ite = ite + 1
 
     if args.log_hist:
-        var_sum = sess.run(merged_sum, feed_dict=input_feed)
+        var_sum = sess.run(model['merged_sum'], feed_dict=input_feed)
         writer.add_summary(var_sum, ite)
     return loss_meter.avg, ite
 
-def evaluate(x, y, args, sess, ite=None, writer=None, name=''):
+def evaluate(model, x, y, args, sess, ite=None, writer=None, name=''):
     loss_meter = AverageMeter()
     output_all = np.array([]).reshape((-1, 1))
     for i in range(0, len(y), args.batch_size):
@@ -137,13 +145,13 @@ def evaluate(x, y, args, sess, ite=None, writer=None, name=''):
             batch_x = np.concatenate((batch_x, np.zeros((args.batch_size - n_flows, batch_x.shape[1]))), axis=0)
             batch_y = np.concatenate((batch_y, np.zeros(args.batch_size - n_flows)), axis=0)
         input_feed = {
-            feat: batch_x,
-            labels: batch_y,
-            learning_rate: args.lr,
-            data_len: n_flows,
-            keep_probs: np.ones(len(args.keep_probs)),
+            model['feat']: batch_x,
+            model['labels']: batch_y,
+            model['learning_rate']: args.lr,
+            model['data_len']: n_flows,
+            model['keep_probs']: np.ones(len(args.keep_probs)),
             }
-        loss_b, output_b = sess.run([loss, output], feed_dict=input_feed)
+        loss_b, output_b = sess.run([model['loss'], model['output']], feed_dict=input_feed)
         loss_meter.update(loss_b)
         output_all = np.concatenate((output_all, output_b[:n_flows].reshape((-1, 1))))
 
@@ -151,7 +159,7 @@ def evaluate(x, y, args, sess, ite=None, writer=None, name=''):
         write_summary(writer, '%s loss' % name, loss_meter.avg, ite)
     return loss_meter.avg, output_all
 
-def run_training(train_x, train_y, valid_x, valid_y, test_x, test_y, args, sess, summary_writer):
+def run_training(model, train_x, train_y, valid_x, valid_y, test_x, test_y, args, sess, summary_writer):
 
     n_batch_per_ep = len(train_y) // args.batch_size
     ite = args.start_epoch * n_batch_per_ep + 1
@@ -159,13 +167,13 @@ def run_training(train_x, train_y, valid_x, valid_y, test_x, test_y, args, sess,
 
     for ep in range(args.start_epoch, args.n_epochs):
         start_t = time.time()
-        train_loss, ite = train(train_x, train_y, args, sess, ite, summary_writer)
+        train_loss, ite = train(model, train_x, train_y, args, sess, ite, summary_writer)
         train_time = time.time() - start_t
 
         if ep % args.eval_n_epochs == 0:
             start_t = time.time()
-            valid_loss, valid_output  = evaluate(valid_x, valid_y, args, sess, ite, summary_writer, name='valid')
-            test_loss, test_output = evaluate(test_x, test_y, args, sess, ite, summary_writer, name='test')
+            valid_loss, valid_output = evaluate(model, valid_x, valid_y, args, sess, ite, summary_writer, name='valid')
+            test_loss, test_output   = evaluate(model, test_x, test_y, args, sess, ite, summary_writer, name='test')
             eval_time = time.time() - start_t
 
             # save the best model from validation
@@ -185,19 +193,17 @@ def run_training(train_x, train_y, valid_x, valid_y, test_x, test_y, args, sess,
                     )
                 keep_latest_files(folder, n_keep=3)
 
-            res = ("epoch %d, thr %d, training loss %.4f (%.1f sec), "
+            res = ("epoch %d, training loss %.4f (%.1f sec), "
                     "valid loss %.4f, test loss %.4f (%.1f sec)") % \
-                    (ep, args.hh_thr, train_loss, train_time, valid_loss, test_loss, eval_time)
+                    (ep, train_loss, train_time, valid_loss, test_loss, eval_time)
         else:
-            res = 'epoch %d, thr %d, training loss %.4f (%.1f sec)' % \
-                    (ep, args.hh_thr, train_loss, train_time)
+            res = 'epoch %d, training loss %.4f (%.1f sec)' % \
+                    (ep, train_loss, train_time)
         print(res)
         fp.write(res+'\n')
         fp.flush()
         summary_writer.flush()
 
-    # finish training - print summary
-    # TODO: load best model and evaluate it
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(sys.argv[0])
@@ -205,29 +211,26 @@ if __name__ == '__main__':
     argparser.add_argument("--valid", type=str, nargs='*', help="validation data (.npy file)", default="")
     argparser.add_argument("--test",  type=str, nargs='*', help="testing data (.npy file)", required=True)
     argparser.add_argument("--save_name", type=str, help="name for the save results", required=True)
-    argparser.add_argument("--seed", type=int, help="random state for sklearn", default=69)
+    argparser.add_argument("--seed", type=int, help="random seed", default=69)
     argparser.add_argument("--n_epochs", type=int, help="number of epochs for training", default=1)
-    argparser.add_argument("--eval_n_epochs", type=int, help="number of epochs to evaluate", default=20)
-    argparser.add_argument("--batch_size", type=int, help="batch size for training", default=256)
-    argparser.add_argument('--hiddens', type=int, nargs='*', default=[100, 100], help="Examples: --hiddnes 144 288")
-    argparser.add_argument('--rnn_hiddens', type=int, nargs='*', default=[100, 100], help="Examples: --hiddnes 144 288")
-    argparser.add_argument('--embed_size', type=int, default=32)
-    argparser.add_argument('--keep_probs', type=float, nargs='*', default=[], help="Examples: --keep_probs 1 0.9 0.9")
+    argparser.add_argument("--eval_n_epochs", type=int, help="inference on validation and test set every eval_n_epochs epochs", default=20)
+    argparser.add_argument("--batch_size", type=int, help="batch size for training", default=128)
+    argparser.add_argument('--hiddens', type=int, nargs='*', default=[32], help="# of hidden units for the final layers")
+    argparser.add_argument('--rnn_hiddens', type=int, nargs='*', default=[256], help="# of hidden units for the RNN layers")
+    argparser.add_argument('--embed_size', type=int, default=64)
+    argparser.add_argument('--keep_probs', type=float, nargs='*', default=[], help="dropout probabilities for the final layers")
     argparser.add_argument("--lr", type=float, default = 0.0001, help="learning rate")
     argparser.add_argument("--memory", type=float, default = 1.0, help="GPU memory fraction used for model training")
-    argparser.add_argument('--resume_training', type=str, default="")
-    argparser.add_argument('--start_epoch', type=int, default=0)
-    argparser.add_argument('--hh_thr', type=int, default=0)
-    argparser.add_argument('--activation', type=str, default="LeakyReLU")
-    argparser.add_argument('--evaluate', action='store_true', default=False)
-    argparser.add_argument('--log_hist', action='store_true', default=False)
+    argparser.add_argument('--resume_training', type=str, default="", help="Path to a model checkpoint. Use this flag to resume training or run inference.")
+    argparser.add_argument('--start_epoch', type=int, default=0, help="For checkpoint and summary logging. Specify this to be the epoch number of the loaded checkpoint +1.")
+    argparser.add_argument('--activation', type=str, default="LeakyReLU", help="activation for FC layers")
+    argparser.add_argument('--evaluate', action='store_true', default=False, help="Run model evaluation without training.")
+    argparser.add_argument('--log_hist', action='store_true', default=False, help="log histogram of gradients during training for debuging")
     argparser.add_argument('--regress_actual', action='store_true', default=False)
     argparser.add_argument("--regress_min", type=float, default=1, help="minimum cutoff for regression")
-    argparser.add_argument('--subsamp_test', type=int, default=0)
-    argparser.add_argument('--word_max_len', type=int, default=30)
+    argparser.add_argument('--word_max_len', type=int, default=60)
     argparser.add_argument('--cell', type=str, default='LSTM')
     argparser.add_argument('--relu_output', action='store_true', default=False)
-    argparser.add_argument('--pctg_below_hh', type=float, default=0)
     args = argparser.parse_args()
 
     assert args.train != '' or args.resume_training != '',      "Must provide training data or a model"
@@ -263,38 +266,14 @@ if __name__ == '__main__':
     test_x, test_y = get_data_aol_feat_list(args.test)
     print('Load data time %.1f seconds' % (time.time() - start_t))
 
-    if args.subsamp_test:
-        test_x = test_x[::args.subsamp_test]
-        test_y = test_y[::args.subsamp_test]
-
-    data_stat = get_stat('train before hh thr', train_x, train_y)
-    if args.hh_thr:
-        s = 'threshold at %.1f\n' % args.hh_thr
-        print(s)
-        data_stat += s
-        train_x_hh = train_x[train_y > args.hh_thr]
-        train_y_hh = train_y[train_y > args.hh_thr]
-        data_stat += get_stat('train hh', train_x_hh, train_y_hh)
-
-        train_x_sm = train_x[train_y <= args.hh_thr]
-        train_y_sm = train_y[train_y <= args.hh_thr]
-        data_stat += get_stat('train sm', train_x_sm, train_y_sm)
-
-        n_sm = int(len(train_y_sm) * args.pctg_below_hh)
-        train_x = np.concatenate((train_x_hh, train_x_sm[:n_sm]))
-        train_y = np.concatenate((train_y_hh, train_y_sm[:n_sm]))
-        s = 'add %.2f small items #: %d\n' % (args.pctg_below_hh, n_sm)
-        print(s)
-        data_stat += s
-
-    data_stat += get_stat('train before log', train_x, train_y)
-    if not args.regress_actual:
+    data_stat = get_stat('train before log', train_x, train_y)
+    if args.regress_actual:
+        rmin = args.regress_min
+    else:
         train_y = np.log(train_y)
         valid_y = np.log(valid_y)
         test_y = np.log(test_y)
         rmin = np.log(args.regress_min)
-    else:
-        rmin = args.regress_min
 
     data_stat += get_stat('train before rmin', train_x, train_y)
     s = 'rmin %.2f, # train_y < min %.2f\n\n' % (rmin, np.sum(train_y < rmin))
@@ -309,7 +288,7 @@ if __name__ == '__main__':
     data_stat += get_stat('valid', valid_x, valid_y)
     data_stat += get_stat('test', test_x, test_y)
 
-    feat, labels, learning_rate, update_op, loss, merged_sum, data_len, keep_probs, output = construct_graph(args)
+    model = construct_graph(args)
     init = tf.global_variables_initializer()
     best_saver = tf.train.Saver(tf.global_variables(), max_to_keep=3)
 
@@ -332,13 +311,13 @@ if __name__ == '__main__':
             print(s)
 
         if not args.evaluate:
-            run_training(train_x, train_y, valid_x, valid_y,
+            run_training(model, train_x, train_y, valid_x, valid_y,
                     test_x, test_y, args, sess, summary_writer)
         else:
             start_t = time.time()
-            train_loss, train_output = evaluate(train_x, train_y, args, sess)
-            valid_loss, valid_output = evaluate(valid_x, valid_y, args, sess)
-            test_loss, test_output = evaluate(test_x, test_y, args, sess)
+            train_loss, train_output = evaluate(model, train_x, train_y, args, sess)
+            valid_loss, valid_output = evaluate(model, valid_x, valid_y, args, sess)
+            test_loss, test_output = evaluate(model, test_x, test_y, args, sess)
 
             np.savez(args.save_name+'_res',
                 train_output=train_output,
