18e707fd8b68cf6bd40cb95caa74c5b475e609d4
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-07-09 08:52:48 -0400

    Plot relative error

--- plot_loss.py
+++ plot_loss.py
@@ -32,6 +32,9 @@ opacity = 1
 def loss_weighted(loss, y_true, y_est):
     return loss + np.abs(y_true - y_est) * y_true
 
+def loss_relative(loss, y_true, y_est):
+    return loss + np.abs(y_true - y_est) / y_true
+
 def loss_l1(loss, y_true, y_est):
     return loss + np.abs(y_true - y_est)
 
@@ -39,15 +42,14 @@ def loss_l2(loss, y_true, y_est):
     return loss + np.abs(y_true - y_est) ** 2
 
 # order of functions and labels must match
-loss_functions = [loss_l1, loss_l2, loss_weighted]
-loss_labels = ["Average Loss per item (L1)", "Average Loss (L2)", "Average Loss (Weighted)"]
+loss_functions = [loss_l1, loss_l2, loss_weighted, loss_relative]
+loss_labels = ["Average Loss per item (L1)", "Average Loss (L2)", "Average Loss (Weighted)", "Average Loss (Relative)"]
 ###########################
 
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(sys.argv[0])
-    argparser.add_argument("--results_learned", type=str, default='')
-    argparser.add_argument("--results_cutoff", type=str, default='')
+    argparser.add_argument("--results", type=str, default='')
     argparser.add_argument("--x_lim", type=float, nargs='*', default=[])
     argparser.add_argument("--y_lim", type=float, nargs='*', default=[])
     argparser.add_argument("--title", type=str, default='')
@@ -57,35 +59,28 @@ if __name__ == '__main__':
     argparser.add_argument("--max_space", type=float, default=4.0)
     args = argparser.parse_args()
     
-    save_file_names = ["experiments/loss_l1_ip.pdf", "experiments/loss_l2_ip.pdf", "experiments/loss_weighted_ip.pdf", "experiments/selection_ip.pdf"]
+    save_file_names = ["experiments/loss_l1_ip.pdf", "experiments/loss_l2_ip.pdf", "experiments/loss_weighted_ip.pdf", "experiments/loss_relative_ip.pdf"]
     if args.aol:
-        save_file_names = ["experiments/loss_l1_aol.pdf", "experiments/loss_l2_aol.pdf", "experiments/loss_weighted_aol.pdf", "experiments/selection_aol.pdf"]
+        save_file_names = ["experiments/loss_l1_aol.pdf", "experiments/loss_l2_aol.pdf", "experiments/loss_weighted_aol.pdf", "experiments/loss_relative_aol.pdf"]
     elif args.synth:
-        save_file_names = ["experiments/loss_l1_synth.pdf", "experiments/loss_l2_synth.pdf", "experiments/loss_weighted_synth.pdf", "experiments/selection_synth.pdf"]
+        save_file_names = ["experiments/loss_l1_synth.pdf", "experiments/loss_l2_synth.pdf", "experiments/loss_weighted_synth.pdf", "experiments/loss_relative_synth.pdf"]
        
-    results_regular = np.load(args.results_learned,  allow_pickle=True)
+    results_regular = np.load(args.results,  allow_pickle=True)
     space = np.array(results_regular['space_list'])
     true_counts = np.array(results_regular['true_values'])
     pred_counts = np.array(results_regular['oracle_predictions'])
-    algo_predictions = np.array(results_regular['valid_algo_predictions'])
-    count_sketch_predictions = np.array(results_regular['valid_count_sketch_predictions'])
-    
-    if args.results_cutoff:
-        results_cutoff = np.load(args.results_cutoff,  allow_pickle=True)
-        cutoff_algo_predictions = np.array(results_cutoff['valid_cutoff_algo_predictions'])
-        cutoff_count_sketch_predictions = np.array(results_cutoff['valid_cutoff_count_sketch_predictions'])
-        true_counts2 = np.array(results_cutoff['true_values'])
-        print("num space " + str(len(cutoff_algo_predictions)))
+    algo_predictions = np.array(results_regular['algo_predictions'])
+    count_sketch_predictions = np.array(results_regular['count_sketch_predictions'])
+    cutoff_count_sketch_predictions = np.array(results_regular['cutoff_count_sketch_predictions'])
 
-    else:
-        cutoff_algo_predictions = np.ones(len(algo_predictions))
-        cutoff_count_sketch_predictions = np.ones(len(count_sketch_predictions))
+    print(algo_predictions)
 
     total = sum(true_counts)
     
     space_percent = [math.ceil(x) for x in (space*1e8) / ((len(true_counts)-1) * 4)]
          
     for i in range(len(loss_functions)):
+        loss_constant = []
         loss_sketch = []
         loss_learned =  []
         loss_just_cutoff =  []
@@ -93,26 +88,27 @@ if __name__ == '__main__':
         #loss_learned_perfect_oracle_i = []
 
         for j in range(len(space)):
+            loss_for_space_constant = 0
             loss_for_space_algo = 0
             loss_for_space_sketch = 0
-            loss_for_space_cutoff_algo = 0
             loss_for_space_cutoff_count_sketch = 0
 
+            constant = np.median(true_counts)
             for k in range(len(true_counts)):
+                loss_for_space_constant = loss_functions[i](loss_for_space_constant, true_counts[k], constant)
                 loss_for_space_algo = loss_functions[i](loss_for_space_algo, true_counts[k], algo_predictions[j][k])
                 loss_for_space_sketch = loss_functions[i](loss_for_space_sketch, true_counts[k], count_sketch_predictions[j][k])
-                loss_for_space_cutoff_algo = loss_functions[i](loss_for_space_cutoff_algo, true_counts[k], cutoff_algo_predictions[j][k])
                 loss_for_space_cutoff_count_sketch = loss_functions[i](loss_for_space_cutoff_count_sketch, true_counts[k], cutoff_count_sketch_predictions[j][k])
 
+            loss_constant.append(loss_for_space_constant)
             loss_learned.append(loss_for_space_algo)
             loss_sketch.append(loss_for_space_sketch)
-            loss_learned_cutoff.append(loss_for_space_cutoff_algo)
             loss_just_cutoff.append(loss_for_space_cutoff_count_sketch)
 
+        loss_constant = np.array(loss_constant) / total
         loss_sketch = np.array(loss_sketch) / total
         loss_learned = np.array(loss_learned) / total
         loss_just_cutoff = np.array(loss_just_cutoff) / total
-        loss_learned_cutoff = np.array(loss_learned_cutoff) / total
         #loss_learned_perfect_oracle_i = np.array(loss_learned_perfect_oracle_i) / total
 
 
@@ -121,7 +117,7 @@ if __name__ == '__main__':
         ax.plot(space_percent, loss_sketch, label="Count Sketch", linewidth=3, color=colors[0], zorder=5)
         ax.plot(space_percent, loss_learned, label=args.algo, linewidth=3, color=colors[1], zorder=6)
         ax.plot(space_percent, loss_just_cutoff, label="Cutoff Count Sketch", linestyle='dashdot', color=colors[3])
-        ax.plot(space_percent, loss_learned_cutoff, label="Cutoff " + args.algo , linestyle='dashdot', color=colors[2])
+        ax.plot(space_percent, loss_constant, label="Constant", color=colors[4], zorder=6)
         # ax.plot(space, loss_learned_perfect_oracle_i, label=args.algo + " (perfect oracle)", linestyle='dotted', color=colors[4])
 
 
