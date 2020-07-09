97e2e540ea82055d12d731ac3133c005c0c7a225
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-07-09 09:06:44 -0400

    Resume model from checkpoint

--- neuralnet/run.sh
+++ neuralnet/run.sh
@@ -14,32 +14,32 @@
 #    --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
 #            ../data/equinix-chicago.dirA.20160121-130800.ports.npy \
 #    --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
-#    --save exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 2000 --lr 0.0001 --regress_min 65
+#    --save exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 200 --lr 0.0001 --regress_min 65
 
 # resume training from an earlier model checkpoint
 
-#python3 run_ip_model.py \
-#    --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
-#            ../data/equinix-chicago.dirA.20160121-130100.ports.npy \
-#            ../data/equinix-chicago.dirA.20160121-130200.ports.npy \
-#            ../data/equinix-chicago.dirA.20160121-130300.ports.npy \
-#            ../data/equinix-chicago.dirA.20160121-130400.ports.npy \
-#            ../data/equinix-chicago.dirA.20160121-130500.ports.npy \
-#            ../data/equinix-chicago.dirA.20160121-130600.ports.npy \
-#    --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
-#            ../data/equinix-chicago.dirA.20160121-130800.ports.npy \
-#    --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
-#    --save exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_ep100 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 2000 --lr 0.0001 --regress_min 65 \
-#    --resume model/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_20180505-214726_ep100.69 --start_epoch 101 --eval_n 10
+python3 run_ip_model.py \
+   --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130100.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130200.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130300.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130400.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130500.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130600.ports.npy \
+   --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
+           ../data/equinix-chicago.dirA.20160121-130800.ports.npy \
+   --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
+   --save exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512 --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 200 --lr 0.0001 --regress_min 65 \
+   --resume predictions/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_20200621-153848_ep180.69 --start_epoch 181 --eval_n 10
 
 # inference
 
-python3 run_ip_model.py \
-   --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
-   --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
-   --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
-   --save pred_weighted --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 2000 --lr 0.0001 --regress_min 65 \
-   --resume ../paper_model/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_ep100_20180506-230316_ep350.69 --evaluate
+# python3 run_ip_model.py \
+#    --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
+#    --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
+#    --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
+#    --save pred_weighted --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 200 --lr 0.0001 --regress_min 65 \
+#    --resume ../paper_model/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_ep100_20180506-230316_ep350.69 --evaluate
 
 
 # === Search Query ===
