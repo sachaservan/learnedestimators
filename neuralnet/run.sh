45e22b2003c755744888c67124206fbfa0297fbb
Sacha Servan-Schreiber <sachaservanschreiber@gmail.com>
2020-10-25 10:47:05 -0400

    Resume from previous AOL checkpoint

--- neuralnet/run.sh
+++ neuralnet/run.sh
@@ -34,17 +34,30 @@
 
 # inference
 
-python3 run_ip_model.py \
-    --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
-    --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
-    --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
-    --save pred_weighted --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 200 --lr 0.0001 --regress_min 65 \
-    --resume model/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_20200621-153848_ep180.69 --evaluate
+# python3 run_ip_model.py \
+#     --train ../data/equinix-chicago.dirA.20160121-130000.ports.npy \
+#     --valid ../data/equinix-chicago.dirA.20160121-130700.ports.npy \
+#     --test  ../data/equinix-chicago.dirA.20160121-130900.ports.npy \
+#     --save pred_relative_ip --rnn_hiddens 64 --port_hiddens 16 8 --hiddens 32 32 --batch_size 512 --n_epoch 200 --lr 0.0001 --regress_min 65 \
+#     --resume model/exp20_ip_rnn_10min_r1-p2-h2_rmin65_ru64_bs512_20200621-153848_ep180.69 --evaluate
 
 
 # === Search Query ===
 
 # training
+
+# python3 ./run_aol_model.py \
+#    --train ../data/aol_0000_len60.npz \
+#            ../data/aol_0001_len60.npz \
+#            ../data/aol_0002_len60.npz \
+#            ../data/aol_0003_len60.npz \
+#            ../data/aol_0004_len60.npz \
+#    --valid ../data/aol_0005_len60.npz \
+#    --test  ../data/aol_0006_len60.npz \
+#    --save exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra  --embed_size 64 --rnn_hidden 256 --hiddens 32 --batch_size 128 --n_epoch 500 --lr 0.0001 --word_max_len 60 --regress_actual --eval_n 10
+
+
+# resume training from an earlier model checkpoint
 
 python3 ./run_aol_model.py \
    --train ../data/aol_0000_len60.npz \
@@ -55,6 +68,7 @@ python3 ./run_aol_model.py \
    --valid ../data/aol_0005_len60.npz \
    --test  ../data/aol_0006_len60.npz \
    --save exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra  --embed_size 64 --rnn_hidden 256 --hiddens 32 --batch_size 128 --n_epoch 500 --lr 0.0001 --word_max_len 60 --regress_actual --eval_n 10
+   --resume model/exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra_20201019-092449_ep140.699 --evaluate
 
 # inference 
 tday=06
@@ -62,6 +76,6 @@ python3 ./run_aol_model.py \
    --train ../data/aol_0000_len60.npz \
    --valid ../data/aol_0005_len60.npz \
    --test  ../data/aol_00${tday}_len60.npz \
-   --save aol_inf_all_v05_t${tday}_exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra_20180514-160509_ep190 --embed_size 64 --rnn_hidden 256 --hiddens 32 --batch_size 128 \
+   --save pred_relative_aol --embed_size 64 --rnn_hidden 256 --hiddens 32 --batch_size 128 \
    --n_epoch 2000 --lr 0.0001 --word_max_len 60 --regress_actual \
-   --resume models_aol/exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra_20180514-160509_ep190.69 --evaluate
+   --resume model/exp22_aol_5d_r1-h1_u256-32_eb64_bs128_ra_20180514-160509_ep190.69 --evaluate
